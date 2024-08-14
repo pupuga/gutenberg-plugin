3b2ecd5d402181afc457b6c269b1725c56f32ce9
alex shandor <mvshandor@gmail.com>
2024-08-13 22:11:55 -0400

    fixed -> file names

--- cmd-block-creation.php
+++ cmd-block-creation.php
@@ -3,25 +3,38 @@
 require_once (__DIR__ . '/cmd/CmdCreation.php');
 
 /*
- * block         - block slug
- * title         - block title
- * description   - block description
+ * name          - plugin name
+ * description   - plugin description
+ * wordpress     - WordPress version
+ * php           - php version
  * version       - plugin version
+ * author        - author name
+ * authorLink    - author site link
+ * license       - plugin license
+ * licenseLink   - plugin license link
  */
 $templates = [
-	'block' => 'example-block',
-	'title' => 'Example block',
-	'description' => 'The block helps you simply create a example block.',
-	'version' => '1.0.0',
+	'name'        => 'Gutenberg block name',
+	'description' => 'Gutenberg block description',
+	'wordpress'   => '6.5.4',
+	'php'         => '7.0',
+	'version'     => '0.1.0',
+	'author'      => 'Alex Shandor',
+	'authorLink'  => 'https://github.com/pupuga',
+	'license'     => 'GPL-3.0-or-later',
+	'licenseLink' => 'https://www.gnu.org/licenses/gpl-3.0.html'
 ];
 
 $files = [
-	'block.json',
-	'editor.scss',
-	'render.php'
+	'Init/Blocks.php',
+	'Init/Init.php',
+	'index.php',
+	'readme.txt',
+	'package.json',
+	'composer.json'
 ];
 
 CmdCreation::app()
 	->setTemplates($templates)
 	->setFiles($files)
-	->block();
+	->plugin();
--- cmd-plugin-creation.php
+++ cmd-plugin-creation.php
@@ -3,38 +3,25 @@
 require_once (__DIR__ . '/cmd/CmdCreation.php');
 
 /*
- * name          - plugin name
- * description   - plugin description
- * wordpress     - WordPress version
- * php           - php version
+ * block         - block slug
+ * title         - block title
+ * description   - block description
  * version       - plugin version
- * author        - author name
- * authorLink    - author site link
- * license       - plugin license
- * licenseLink   - plugin license link
  */
 $templates = [
-	'name'        => 'Gutenberg block name',
-	'description' => 'Gutenberg block description',
-	'wordpress'   => '6.5.4',
-	'php'         => '7.0',
-	'version'     => '0.1.0',
-	'author'      => 'Alex Shandor',
-	'authorLink'  => 'https://github.com/pupuga',
-	'license'     => 'GPL-3.0-or-later',
-	'licenseLink' => 'https://www.gnu.org/licenses/gpl-3.0.html'
+	'block' => 'example-block',
+	'title' => 'Example block',
+	'description' => 'The block helps you simply create a example block.',
+	'version' => '1.0.0',
 ];
 
 $files = [
-	'Init/Blocks.php',
-	'Init/Init.php',
-	'index.php',
-	'readme.txt',
-	'package.json',
-	'composer.json'
+	'block.json',
+	'editor.scss',
+	'render.php'
 ];
 
 CmdCreation::app()
 	->setTemplates($templates)
 	->setFiles($files)
-	->plugin();
+	->block();
